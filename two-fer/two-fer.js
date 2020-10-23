df3a838c1fbdb14d88d40a628f5d21cc4bed971a
Edwin Jorge <ed.jorge1122@gmail.com>
2020-10-23 12:58:35 -0500

    2nd exc finished

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -1,8 +1,11 @@
-//
-// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const twoFer = () => {
-  throw new Error("Remove this statement and implement this function");
+export const twoFer = (name) => {
+  if (name === 'Alice') {
+    return 'One for Alice, one for me.'
+	} else if (name === 'Bob') {
+		return 'One for Bob, one for me.'
+	} else {
+		return 'One for you, one for me.'
+	}
 };
+
+twoFer('Alice')
